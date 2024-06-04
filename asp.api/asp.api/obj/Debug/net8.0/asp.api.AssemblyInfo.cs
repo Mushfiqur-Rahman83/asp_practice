44d53a7d5eff5e0cbb7693aacc28818b97eabea6
HP <mushfique845@gmail.com>
2024-06-05 01:11:03 +0600

    Migration Done

--- asp.api/asp.api/obj/Debug/net8.0/asp.api.AssemblyInfo.cs
+++ asp.api/asp.api/obj/Debug/net8.0/asp.api.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("asp.api")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+7d6251d025443e20ae1b51b28a221dd64f328346")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+dfa71696831008736a22e3d082b6396b49ae7b80")]
 [assembly: System.Reflection.AssemblyProductAttribute("asp.api")]
 [assembly: System.Reflection.AssemblyTitleAttribute("asp.api")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
